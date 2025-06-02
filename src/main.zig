7512ec4fd0f06c8b52f994ad0eead2a3da5609ae
Cincidial <cincidialstudios@gmail.com>
2025-06-01 22:01:24 -0600

    Initial window commit with code from example.

---
+++ src/main.zig
@@ -0,0 +1,207 @@
+const std = @import("std");
+const c = @cImport({
+    @cDefine("SDL_DISABLE_OLD_NAMES", {});
+    @cInclude("SDL3/SDL.h");
+    @cInclude("SDL3/SDL_revision.h");
+    @cDefine("SDL_MAIN_HANDLED", {}); // We provide our own entry point to pass off to zig handlers
+    @cInclude("SDL3/SDL_main.h");
+});
+
+const sdl_log = std.log.scoped(.sdl);
+const app_log = std.log.scoped(.app);
+
+var fully_initialized = false;
+const window_w = 640;
+const window_h = 480;
+var window: *c.SDL_Window = undefined;
+var renderer: *c.SDL_Renderer = undefined;
+
+fn fmtSdlDrivers(
+    current_driver: [*:0]const u8,
+    num_drivers: c_int,
+    getDriver: *const fn (c_int) callconv(.C) ?[*:0]const u8,
+) std.fmt.Formatter(formatSdlDrivers) {
+    return .{ .data = .{
+        .current_driver = current_driver,
+        .num_drivers = num_drivers,
+        .getDriver = getDriver,
+    } };
+}
+
+fn formatSdlDrivers(
+    context: struct {
+        current_driver: [*:0]const u8,
+        num_drivers: c_int,
+        getDriver: *const fn (c_int) callconv(.C) ?[*:0]const u8,
+    },
+    comptime _: []const u8,
+    _: std.fmt.FormatOptions,
+    writer: anytype,
+) !void {
+    var i: c_int = 0;
+    while (i < context.num_drivers) : (i += 1) {
+        if (i != 0) {
+            try writer.writeAll(", ");
+        }
+        const driver = context.getDriver(i).?;
+        try writer.writeAll(std.mem.span(driver));
+        if (std.mem.orderZ(u8, context.current_driver, driver) == .eq) {
+            try writer.writeAll(" (current)");
+        }
+    }
+}
+
+/// Converts the return value of an SDL function to an error union.
+inline fn errify(value: anytype) error{SdlError}!switch (@typeInfo(@TypeOf(value))) {
+    .bool => void,
+    .pointer, .optional => @TypeOf(value.?),
+    .int => |info| switch (info.signedness) {
+        .signed => @TypeOf(@max(0, value)),
+        .unsigned => @TypeOf(value),
+    },
+    else => @compileError("unerrifiable type: " ++ @typeName(@TypeOf(value))),
+} {
+    return switch (@typeInfo(@TypeOf(value))) {
+        .bool => if (!value) error.SdlError,
+        .pointer, .optional => value orelse error.SdlError,
+        .int => |info| switch (info.signedness) {
+            .signed => if (value >= 0) @max(0, value) else error.SdlError,
+            .unsigned => if (value != 0) value else error.SdlError,
+        },
+        else => comptime unreachable,
+    };
+}
+
+fn sdlAppInit(_: ?*?*anyopaque, _: [][*:0]u8) !c.SDL_AppResult {
+    sdl_log.debug("SDL build time version: {d}.{d}.{d}", .{
+        c.SDL_MAJOR_VERSION,
+        c.SDL_MINOR_VERSION,
+        c.SDL_MICRO_VERSION,
+    });
+    sdl_log.debug("SDL build time revision: {s}", .{c.SDL_REVISION});
+    {
+        const version = c.SDL_GetVersion();
+        sdl_log.debug("SDL runtime version: {d}.{d}.{d}", .{
+            c.SDL_VERSIONNUM_MAJOR(version),
+            c.SDL_VERSIONNUM_MINOR(version),
+            c.SDL_VERSIONNUM_MICRO(version),
+        });
+        const revision: [*:0]const u8 = c.SDL_GetRevision();
+        sdl_log.debug("SDL runtime revision: {s}", .{revision});
+    }
+
+    try errify(c.SDL_SetAppMetadata("Zig Zag", "0.0.1", "zig_zag"));
+    try errify(c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO | c.SDL_INIT_GAMEPAD));
+
+    sdl_log.debug("SDL video drivers: {}", .{fmtSdlDrivers(
+        c.SDL_GetCurrentVideoDriver().?,
+        c.SDL_GetNumVideoDrivers(),
+        c.SDL_GetVideoDriver,
+    )});
+    sdl_log.debug("SDL audio drivers: {}", .{fmtSdlDrivers(
+        c.SDL_GetCurrentAudioDriver().?,
+        c.SDL_GetNumAudioDrivers(),
+        c.SDL_GetAudioDriver,
+    )});
+
+    errify(c.SDL_SetHint(c.SDL_HINT_RENDER_VSYNC, "1")) catch {};
+    try errify(c.SDL_CreateWindowAndRenderer("Speedbreaker", window_w, window_h, 0, @ptrCast(&window), @ptrCast(&renderer)));
+    errdefer c.SDL_DestroyWindow(window);
+    errdefer c.SDL_DestroyRenderer(renderer);
+
+    return c.SDL_APP_CONTINUE;
+}
+
+fn sdlAppIterate(_: ?*anyopaque) !c.SDL_AppResult {
+    return c.SDL_APP_CONTINUE;
+}
+
+fn sdlAppEvent(_: ?*anyopaque, event: *c.SDL_Event) !c.SDL_AppResult {
+    switch (event.type) {
+        c.SDL_EVENT_QUIT => {
+            return c.SDL_APP_SUCCESS;
+        },
+        else => return c.SDL_APP_CONTINUE,
+    }
+}
+
+fn sdlAppQuit(appstate: ?*anyopaque, result: anyerror!c.SDL_AppResult) void {
+    _ = appstate;
+
+    _ = result catch |err| if (err == error.SdlError) {
+        sdl_log.err("{s}", .{c.SDL_GetError()});
+    };
+
+    if (fully_initialized) {
+        c.SDL_DestroyRenderer(renderer);
+        c.SDL_DestroyWindow(window);
+        c.SDL_Quit();
+        fully_initialized = false;
+    }
+}
+
+pub fn main() !u8 {
+    app_err.reset();
+    var empty_argv: [0:null]?[*:0]u8 = .{};
+    const status: u8 = @truncate(@as(c_uint, @bitCast(c.SDL_RunApp(empty_argv.len, @ptrCast(&empty_argv), sdlMainC, null))));
+    return app_err.load() orelse status;
+}
+
+fn sdlMainC(argc: c_int, argv: ?[*:null]?[*:0]u8) callconv(.c) c_int {
+    return c.SDL_EnterAppMainCallbacks(argc, @ptrCast(argv), sdlAppInitC, sdlAppIterateC, sdlAppEventC, sdlAppQuitC);
+}
+
+fn sdlAppInitC(appstate: ?*?*anyopaque, argc: c_int, argv: ?[*:null]?[*:0]u8) callconv(.c) c.SDL_AppResult {
+    return sdlAppInit(appstate.?, @ptrCast(argv.?[0..@intCast(argc)])) catch |err| app_err.store(err);
+}
+
+fn sdlAppIterateC(appstate: ?*anyopaque) callconv(.c) c.SDL_AppResult {
+    return sdlAppIterate(appstate) catch |err| app_err.store(err);
+}
+
+fn sdlAppEventC(appstate: ?*anyopaque, event: ?*c.SDL_Event) callconv(.c) c.SDL_AppResult {
+    return sdlAppEvent(appstate, event.?) catch |err| app_err.store(err);
+}
+
+fn sdlAppQuitC(appstate: ?*anyopaque, result: c.SDL_AppResult) callconv(.c) void {
+    sdlAppQuit(appstate, app_err.load() orelse result);
+}
+
+var app_err: ErrorStore = .{};
+const ErrorStore = struct {
+    const status_not_stored = 0;
+    const status_storing = 1;
+    const status_stored = 2;
+
+    status: c.SDL_AtomicInt = .{},
+    err: anyerror = undefined,
+    trace_index: usize = undefined,
+    trace_addrs: [32]usize = undefined,
+
+    fn reset(es: *ErrorStore) void {
+        _ = c.SDL_SetAtomicInt(&es.status, status_not_stored);
+    }
+
+    fn store(es: *ErrorStore, err: anyerror) c.SDL_AppResult {
+        if (c.SDL_CompareAndSwapAtomicInt(&es.status, status_not_stored, status_storing)) {
+            es.err = err;
+            if (@errorReturnTrace()) |src_trace| {
+                es.trace_index = src_trace.index;
+                const len = @min(es.trace_addrs.len, src_trace.instruction_addresses.len);
+                @memcpy(es.trace_addrs[0..len], src_trace.instruction_addresses[0..len]);
+            }
+            _ = c.SDL_SetAtomicInt(&es.status, status_stored);
+        }
+        return c.SDL_APP_FAILURE;
+    }
+
+    fn load(es: *ErrorStore) ?anyerror {
+        if (c.SDL_GetAtomicInt(&es.status) != status_stored) return null;
+        if (@errorReturnTrace()) |dst_trace| {
+            dst_trace.index = es.trace_index;
+            const len = @min(dst_trace.instruction_addresses.len, es.trace_addrs.len);
+            @memcpy(dst_trace.instruction_addresses[0..len], es.trace_addrs[0..len]);
+        }
+        return es.err;
+    }
+};
